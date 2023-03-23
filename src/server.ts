be5251e38e4c2ee83de43630636b3a384a018f17
cmarciao <marciaocassio@gmail.com>
2023-03-22 20:47:19 -0400

    feat: Add database connection

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ app.get("/", (req, res) => {
 
 const port = process.env.SERVER_PORT || 3333;
 
-app.listen(port, () => console.log(`Server started at http://localhost:${3333} 🔥`));
+app.listen(port, () => console.log(`Server started at http://localhost:${port}. 🔥`));
