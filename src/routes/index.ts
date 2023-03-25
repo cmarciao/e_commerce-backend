b6b1cff2325dde227e59d193250ce5abaeb815f8
cmarciao <marciaocassio@gmail.com>
2023-03-24 22:09:09 -0400

    feat: Add auth routes in routes application

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import { authRoutes } from "./auth.routes";
 import { userRoutes } from "./user.routes";
 
 const routes = Router();
 
+routes.use("/", authRoutes);
 routes.use("/users", userRoutes);
 
 export { routes };
