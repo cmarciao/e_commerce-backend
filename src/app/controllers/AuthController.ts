4a633f0808364fdfbbb6d1292379cbb0e624db5b
cmarciao <marciaocassio@gmail.com>
2023-03-26 22:07:32 -0400

    feat: Return the token jwt as the login response

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import { AppError } from "../../errors/AppError";
 
 import { errorListFormatter } from "../../utils/formatter";
 import { emptyPropertyList } from "../../utils/validator";
 
 import AuthsRepository from "../repositories/AuthsRepository";
+
+dotenv.config();
 
 class AuthController {
     async login(req: Request, res: Response): Promise<Response> {
@@ -24,7 +28,10 @@ class AuthController {
             return res.status(404).json("Email or password is incorrect.");
         }
 
-        return res.status(200).json("User logged in successfully");
+        const {id} = isUserExists;
+        const token = jwt.sign({ id }, `${process.env.JWT_SECRET_KEY}`);
+
+        return res.status(200).json(token);
     }
 }
 
