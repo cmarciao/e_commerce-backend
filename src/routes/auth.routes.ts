8cc04f68dde186c51656c4ca836900c1452e2ac8
cmarciao <marciaocassio@gmail.com>
2023-03-26 22:08:13 -0400

    feat: Updates the login route path

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,6 +3,6 @@ import AuthController from "../app/controllers/AuthController";
 
 const authRoutes = Router();
 
-authRoutes.post('/login', AuthController.login);
+authRoutes.post('/', AuthController.login);
 
 export { authRoutes };
