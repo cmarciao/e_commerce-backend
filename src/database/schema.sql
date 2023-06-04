7a894ef4a105210ccf88136915d60fd64e629c37
Cássio Marcião <marciaocassio@gmail.com>
2023-06-04 20:33:45 -0300

    feat: Add products sql schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -6,5 +6,13 @@ CREATE TABLE IF NOT EXISTS users (
     id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
     name VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
-    password VARCHAR NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    name VARCHAR NOT NULL,
+    url_photo VARCHAR NOT NULL,
+    price FLOAT NOT NULL,
+    stock_quantity INT NOT NULL
 );
