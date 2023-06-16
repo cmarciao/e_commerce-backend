c6dee85b7f100fc91f60e3bac7269dfae7928f41
Cássio Marcião <marciaocassio@gmail.com>
2023-06-15 23:07:25 -0300

    feat: Add carts and cart_items tables in sql schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
     password VARCHAR NOT NULL
+    created_at DATE NOT NULL DEFAULT now(),
+    updated_at DATE NOT NULL DEFAULT now(),
 );
 
 CREATE TABLE IF NOT EXISTS products (
@@ -14,5 +16,29 @@ CREATE TABLE IF NOT EXISTS products (
     name VARCHAR NOT NULL,
     url_photo VARCHAR NOT NULL,
     price FLOAT NOT NULL,
-    stock_quantity INT NOT NULL
+    stock_quantity INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+);
+
+CREATE TABLE carts (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    amount INT NOT NULL,
+    total FLOAT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    finished_at TIMESTAMP,
+    user_id UUID UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE cart_items (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    product_id UUID,
+    cart_id UUID,
+
+    FOREIGN KEY(product_id) REFERENCES products(id),
+    FOREIGN KEY(cart_id) REFERENCES carts(id)
 );
