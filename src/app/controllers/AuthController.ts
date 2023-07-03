4ddacee00364bc87ae280ca534b4ba24e929a2e6
Cássio Marcião <marciaocassio@gmail.com>
2023-07-02 22:29:30 -0300

    feat: Update AuthController

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -1,45 +1,21 @@
 import { Request, Response } from "express";
-import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
-import { AppError } from "../errors/AppError";
-
-import { errorListFormatter } from "../utils/formatter";
-import { emptyPropertyList } from "../utils/validator";
-
-import UsersRepository from "../repositories/UsersRepository";
-import { compareEncrypt } from "../utils/crypt";
-
-dotenv.config();
+import AuthService from "../services/AuthService";
 
 class AuthController {
     async store(req: Request, res: Response): Promise<Response> {
         const { email, password } = req.body;
 
-        const errorsList = emptyPropertyList({ email, password });
-
-        if(errorsList.length) {
-            const commentError = errorListFormatter(errorsList);
-            
-            throw new AppError(commentError);
-        }
-
-        const userByEmail = await UsersRepository.findByEmail(email);
-
-        if(!userByEmail) {
-            return res.status(404).json("Email or password is incorrect.");
-        }
-
-        const isValidPassword = await compareEncrypt(password, userByEmail.password);
-
-        if(!isValidPassword) {
-            throw new AppError('Email or password is invalid.', 404);
-        }
-        
-
-        const {id} = userByEmail;
-        const token = jwt.sign({ id }, `${process.env.JWT_SECRET_KEY}`);
+        const token = await AuthService.loginByEmailAndPassword(email, password);
 
         return res.status(200).json(token);
+    }
+
+    async show(req: Request, res: Response) {
+        const token = req.headers.authorization || '';
+        
+        const user = await AuthService.getCurrentLoggedUser(token);
+        
+        return res.status(200).json(user);
     }
 }
 
