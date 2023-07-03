f11ae5ce691ec9bcd0793c0f5ef45e30595a9b7f
Cássio Marcião <marciaocassio@gmail.com>
2023-07-02 22:30:24 -0300

    feat: Update ProductService

--- src/app/services/ProductService.ts
+++ src/app/services/ProductService.ts
@@ -1,7 +1,26 @@
 import { AppError } from "../errors/AppError";
+import Product from "../models/Product";
 import ProductsRepository from "../repositories/ProductsRepository";
 
+type ICreateProduct = Omit<Product, 'id'>
+
 class ProductService {
+    async listProducts() {
+        const products = await ProductsRepository.findAll();
+        return products;
+    }
+
+    async createProduct({ name, price, url_photo, stock_quantity}: ICreateProduct) { 
+        const product = await ProductsRepository.create({
+            name,
+            price,
+            url_photo,
+            stock_quantity
+        });
+
+        return product;
+    }
+
     async isProductsExists(product_ids: string[]) {
         for(const productId of product_ids) {
             const product = await ProductsRepository.findById(productId);
