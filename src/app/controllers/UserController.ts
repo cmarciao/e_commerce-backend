f54196fe1555b68866ed56bf8af454b0ec4f4e50
Cássio Marcião <marciaocassio@gmail.com>
2023-07-02 22:30:03 -0300

    feat: Update UserController

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,41 +1,11 @@
 import { Request, Response } from "express";
-import { AppError } from "../errors/AppError";
-
-import { encryptData } from "../utils/crypt";
-import { errorListFormatter } from "../utils/formatter";
-import { emptyPropertyList } from "../utils/validator";
-
-import UsersRepository from "../repositories/UsersRepository";
+import UserService from "../services/UserService";
 
 class UserController {
-    async index(req: Request, res: Response) {
-        const user = await UsersRepository.findAll();
-        return res.status(200).json(user);
-    }
-
     async store(req: Request, res: Response) {
         const { name, email, password } = req.body;
 
-        const errorsList = emptyPropertyList({ name, email, password });
-
-        if(errorsList.length) {
-            const commentError = errorListFormatter(errorsList);
-            
-            throw new AppError(commentError);
-        }
-
-        const userAlreadyExists = await UsersRepository.findByEmail(email);
-        
-        if(userAlreadyExists) {
-            throw new AppError("User already exists.");
-        }
-        
-        const encryptedPassword = await encryptData(password);
-        const user = await UsersRepository.create({
-            name,
-            email,
-            password: encryptedPassword
-        });
+        const user = await UserService.createUser(name, email, password);
 
         return res.status(201).json(user);
     }
