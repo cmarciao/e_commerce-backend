2a27fefacac6d1182f4373dfe646654bacfb58e1
Cássio Marcião <marciaocassio@gmail.com>
2023-07-02 22:29:39 -0300

    feat: Update CartController

--- src/app/controllers/CartController.ts
+++ src/app/controllers/CartController.ts
@@ -1,40 +1,74 @@
 import { Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+
 import CartService from "../services/CartService";
+import { AppError } from "../errors/AppError";
+
+interface DecodedToken {
+    id: string;
+    iat: number;
+}
 
 class CartController {
     async show(req: Request, res: Response) {
-        const user_id = req.headers['x-user-id'] as string;
-
-        const cart = await CartService.getCartByUserId(user_id);
-
-        return res.status(200).json(cart);
+        const tokenHeader = req.headers.authorization;
+        
+        try {
+            const token = tokenHeader?.split(' ')[1] || '';
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY || '') as DecodedToken;
+            const cart = await CartService.getCartByUserId(decoded.id);
+            
+            return res.status(200).json(cart);
+        } catch {
+            throw new AppError('Invalid token', 401);
+        }
     }
-
+    
     async addProduct(req: Request, res: Response) {
-        const user_id = req.headers['x-user-id'] as string;
         const { product_ids } = req.body;
-
-        const cart = await CartService.addProducts(user_id, product_ids);
-
-        return res.status(201).json(cart);
+        const tokenHeader = req.headers.authorization;
+        
+        try {
+            const token = tokenHeader?.split(' ')[1] || '';
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY || '') as DecodedToken;
+            const cart = await CartService.addProducts(decoded.id, product_ids);
+    
+            return res.status(201).json(cart);
+        } catch {
+            throw new AppError('Invalid token', 401);
+        }
     }
 
     async removeProduct(req: Request, res: Response) {
-        const user_id = req.headers['x-user-id'] as string;
         const { product_id } = req.body;
+        const tokenHeader = req.headers.authorization;
+        
+        try {
+            const token = tokenHeader?.split(' ')[1] || '';
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY || '') as DecodedToken;
 
-        const cart = await CartService.removeProduct(user_id, product_id)
+            const cart = await CartService.removeProduct(decoded.id, product_id)
 
-        return res.status(200).json(cart);
+            return res.status(200).json(cart);
+        } catch {
+            throw new AppError('Invalid token', 401);
+        }
     }
     
     async removeCartItem(req: Request, res: Response) {
-        const user_id = req.headers['x-user-id'] as string;
         const { product_id } = req.body;
+        const tokenHeader = req.headers.authorization;
+        
+        try {
+            const token = tokenHeader?.split(' ')[1] || '';
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY || '') as DecodedToken;
 
-        const cart = await CartService.removeCartItem(user_id, product_id)
+            const cart = await CartService.removeCartItem(decoded.id, product_id)
 
-        return res.status(200).json(cart);
+            return res.status(200).json(cart);
+        } catch {
+            throw new AppError('Invalid token', 401);
+        }
     }
 }
 
